bf05ac2031d1467cf4e168477e342dd66c178540
aivcs <aivcs@users.noreply.github.com>
2026-06-20 02:23:10 -0500

    Revert "chore: point Nix substituter at nix-cache.lornu.ai (#103)"
    This reverts commit 600da6b20b89b96196d49bd30ba22ba5c31bf134.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "local-ci - lightweight local CI runner";
 
   nixConfig = {
-    extra-substituters = [ "https://nix-cache.lornu.ai" ];
-    extra-trusted-public-keys = [ "lornu-1:FSWe0oOoYoYzbDU3XsZOoUz6LYouAKynidEOop1Q8yc=" ];
+    extra-substituters = [ "https://nix-cache.stevedores.org" ];
+    extra-trusted-public-keys = [ "stevedores-cache-1:bXLxkipycRWproIJnk8pPWNFdgVfeV+I2mJXCoW4/ag=" ];
   };
 
   inputs = {
